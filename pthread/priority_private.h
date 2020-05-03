64342004249a830b51d1e1572c097059f483f289
William Kent <wjk011@gmail.com>
2020-05-02 23:06:00 -0400

    Add content to priority_private.h

--- pthread/priority_private.h
+++ pthread/priority_private.h
@@ -5,4 +5,21 @@
 
 typedef uint64_t pthread_priority_t;
 
+static inline pthread_priority_t _pthread_unspecified_priority(void) {
+	return 0;
+}
+
+static inline integer_t _pthread_priority_thread_qos(pthread_priority_t priority) {
+	return (priority >> 32) & 0xFFFFFFFF;
+}
+
+static inline integer_t _pthread_priority_relpri(pthread_priority_t priority) {
+	return priority & 0xFFFFFFFF;
+}
+
+static inline pthread_priority_t _pthread_priority_make_from_thread_qos(integer_t tier, integer_t priority, pthread_priority_t unk __unused) {
+	uint64_t tier64 = (uint64_t)tier;
+	return (tier64 << 32) | priority;
+}
+
 #endif
